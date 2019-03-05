5c969735c90edb70b919ec43ecbdcc3533ef20c9
yoonwonyoul <yoonwonyoul@webchemist.net>
2019-03-06 01:37:14 +0900

    feat: add sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="sidebar">
+    <router-link to="/">관리함</router-link>
+    <router-link to="/today">오늘</router-link>
+    <router-link to="/week">다음 7일</router-link>
+  </div>
+</template>
